cbe7daad7f8b7e5383f438e3b35e6a833923168b
kvrancic <kvrancic11@gmail.com>
2024-05-31 17:29:35 +0200

    further work on vendor dashboard

---
+++ app/vendor/dashboard/NewOfferButton.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import { IconArrowRight } from '@tabler/icons-react';
+
+const PostNewOfferButton = () => {
+  return (
+    <Link href="/vendor/post-new-offer" className="group relative inline-block w-full" passHref>
+        <div className="border-2 rounded-md border-orange-500 text-orange-500 font-bold text-3xl py-8 px-6 w-full flex items-center justify-between group transition-transform transform group-hover:scale-105 group-hover:bg-orange-100">
+            Post New Offer
+            <div className="flex items-center transition-transform transform group-hover:translate-x-4">
+            <IconArrowRight size={32}  className='bg-color-orange-500'/>
+            </div>
+        </div>
+    </Link>
+  );
+};
+
+export default PostNewOfferButton;
--- app/vendor/dashboard/VendorGrid.jsx
+++ app/vendor/dashboard/VendorGrid.jsx
@@ -6,12 +6,15 @@ import { useDisclosure } from '@mantine/hooks';
 import { createClient } from '@/utils/supabase/client';
 import Avatar from '@/components/Avatar';
 import Link from 'next/link';
-import { IconPlus, IconEdit, IconLogout } from '@tabler/icons-react';
+import { IconPlus, IconEdit, IconLogout, IconArrowRight } from '@tabler/icons-react';
+import { StatsRing } from './StatsRing'; 
+import NewOfferButton from './NewOfferButton'
 
 export function VendorGrid() {
   const supabase = createClient();
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [statsData, setStatsData] = useState([]);
   const [opened, { toggle, close }] = useDisclosure(false);
 
   useEffect(() => {
@@ -34,6 +37,39 @@ export function VendorGrid() {
     fetchUser();
   }, [supabase]);
 
+  useEffect(() => {
+    const fetchStats = async () => {
+      if (user) {
+        const [{ count: availableCount }, { count: unavailableCount }, { count: requestsCount }] = await Promise.all([
+          supabase
+            .from('offering')
+            .select('*', { count: 'exact' })
+            .eq('vendor_id', user.id)
+            .eq('is_available', true),
+          supabase
+            .from('offering')
+            .select('*', { count: 'exact' })
+            .eq('vendor_id', user.id)
+            .eq('is_available', false),
+          supabase
+            .from('offer')
+            .select('*', { count: 'exact' })
+            .eq('vendor_id', user.id)
+        ]);
+
+        console.log('Data received: ' + availableCount + ' ' + unavailableCount + ' ' + requestsCount )
+
+        setStatsData([
+          { label: 'Available Offerings', stats: availableCount, progress: (availableCount / (availableCount + unavailableCount)) * 100 || 0, color: 'teal', icon: 'up' },
+          { label: 'Unavailable Offerings', stats: unavailableCount, progress: (unavailableCount / (availableCount + unavailableCount)) * 100 || 0, color: 'red', icon: 'down' },
+          { label: 'Total Requests', stats: requestsCount, progress: 100, color: 'blue', icon: 'up' },
+        ]);
+      }
+    };
+
+    fetchStats();
+  }, [user, supabase]);
+
   if (loading) {
     return <div className="flex items-center justify-center h-screen">Loading...</div>;
   }
@@ -51,61 +87,51 @@ export function VendorGrid() {
     <div className='w-full p-10 mt-10'>
       <Grid gutter="xs">
         <Grid.Col span={{ base: 12, xs: 4 }}>
-        <Card shadow="lg" p="lg" className="bg-white rounded-lg shadow-lg p-6 transition duration-300 hover:shadow-xl transform hover:-translate-y-1">
-          <div className="flex flex-col items-center space-y-4">
-            <Avatar uid={user.id} url={`https://duelkbjyxfgctjrijjoe.supabase.co/storage/v1/object/public/avatars/${user.picture_url}`} size={150} editingAllowed={false} />
-            <div className="w-full bg-orange-600 p-4 rounded-b-lg">
-              <h1 className="text-2xl font-bold text-white text-center">{user.name}</h1>
+          <Card shadow="lg" p="lg" className="bg-white rounded-lg shadow-lg p-6 transition duration-300 hover:shadow-xl transform hover:-translate-y-1">
+            <div className="flex flex-col items-center space-y-4">
+              <Avatar uid={user.id} url={`https://duelkbjyxfgctjrijjoe.supabase.co/storage/v1/object/public/avatars/${user.picture_url}`} size={150} editingAllowed={false} />
+              <div className="w-full bg-orange-500 p-4 rounded-b-lg">
+                <h1 className="text-2xl font-bold text-white text-center">{user.name}</h1>
+              </div>
             </div>
-          </div>
-        </Card>
-
+          </Card>
         </Grid.Col>
+
         <Grid.Col span={{ base: 12, xs: 8 }} className="">
-            <div  className='hidden lg:flex mt-[180px]  justify-end gap-12 px-20' >
-              <Link href="/vendor/create-offering" passHref className='flex gap-1 text-orange-500 text-md font-bold text-justify items-end hover:text-blue-500 hover:scale-105'>
-                <IconPlus />
-                Create New Offering
-              </Link>
-              <Link href="/edit-profile" passHref className='flex gap-1 text-orange-500 text-md font-bold text-justify items-end hover:text-blue-500 hover:scale-105'>
-                <IconEdit />
-                Edit Profile
-              </Link>
-              <Button
-                onClick={handleSignOut}
-                className="bg-orange-500 hover:bg-blue-600 transition-transform transform hover:scale-105"
-              >
-                <IconLogout />
-                Sign Out
-              </Button>
-            </div>
+          <div className='hidden lg:flex mt-[180px] justify-end gap-12 px-20'>
+            <Link href="/vendor/create-offering" passHref className='flex gap-1 text-orange-500 text-md font-bold text-justify items-end hover:text-blue-500 hover:scale-105'>
+              <IconPlus />
+              Create New Offering
+            </Link>
+            <Link href="/edit-profile" passHref className='flex gap-1 text-orange-500 text-md font-bold text-justify items-end hover:text-blue-500 hover:scale-105'>
+              <IconEdit />
+              Edit Profile
+            </Link>
+            <Button
+              onClick={handleSignOut}
+              className="bg-orange-500 hover:bg-blue-600 transition-transform transform hover:scale-105"
+            >
+              <IconLogout />
+              Sign Out
+            </Button>
+          </div>
         </Grid.Col>
 
-
-        <Grid.Col span={{ base: 12, xs: 8 }}>
+        <Grid.Col span={{ base: 12, xs: 7 }}>
+          <StatsRing data={statsData} />
+        </Grid.Col>
+        <Grid.Col span={{ base: 12, xs: 5 }}>
           <Card shadow="md" p="lg" className="bg-white rounded-lg shadow-lg p-4 transition duration-300 hover:shadow-xl">
             <Text>Other content</Text>
           </Card>
         </Grid.Col>
-        <Grid.Col span={{ base: 12, xs: 4 }}>
+        <Grid.Col span={{ base: 12, xs: 9 }}>
           <Card shadow="md" p="lg" className="bg-white rounded-lg shadow-lg p-4 transition duration-300 hover:shadow-xl">
             <Text>Other content</Text>
           </Card>
         </Grid.Col>
         <Grid.Col span={{ base: 12, xs: 3 }}>
-          <Card shadow="md" p="lg" className="bg-white rounded-lg shadow-lg p-4 transition duration-300 hover:shadow-xl">
-            <Text>Other content</Text>
-          </Card>
-        </Grid.Col>
-        <Grid.Col span={{ base: 12, xs: 3 }}>
-          <Card shadow="md" p="lg" className="bg-white rounded-lg shadow-lg p-4 transition duration-300 hover:shadow-xl">
-            <Text>Other content</Text>
-          </Card>
-        </Grid.Col>
-        <Grid.Col span={{ base: 12, xs: 6 }}>
-          <Card shadow="md" p="lg" className="bg-white rounded-lg shadow-lg p-4 transition duration-300 hover:shadow-xl">
-            <Text>Other content</Text>
-          </Card>
+          <NewOfferButton/>
         </Grid.Col>
       </Grid>
 
@@ -134,7 +160,6 @@ export function VendorGrid() {
           <Link href="/edit-profile" passHref>
             <Button
               component="a"
-              leftIcon={<IconEdit />}
               className="bg-orange-500 hover:bg-blue-600 transition-transform transform hover:scale-105"
             >
               Edit Profile
